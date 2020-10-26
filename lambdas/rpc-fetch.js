c5bf9df3bf66d7709e4e4edb36925c01a4f80808
sam <sam@freighttrust.com>
2020-10-26 16:16:25 -0700

    feat(release): v2.0.0
    this release includes the cannonical EIP's of which RPC/GraphQL implementations use. It also
    includes better documentation and references to the most up-to-date versions of the EIPs in the
    `specification` folder

--- lambdas/rpc-fetch.js
+++ lambdas/rpc-fetch.js
@@ -1,14 +1,16 @@
-await fetch(new Request("https://cloudflare-eth.com", {
-	method: "POST",
-	body: JSON.stringify({
-		"jsonrpc":"2.0",
-		"method":"eth_getBlockByNumber",
-		"params":["0x2244", true],
-		"id":64
-	}),
-	headers: {
-		"Content-Type": "application/json"
-	}
-})).then((resp) => {
-	return resp.json()
-});
+await fetch(
+  new Request("https://cloudflare-eth.com", {
+    method: "POST",
+    body: JSON.stringify({
+      jsonrpc: "2.0",
+      method: "eth_getBlockByNumber",
+      params: ["0x2244", true],
+      id: 64
+    }),
+    headers: {
+      "Content-Type": "application/json"
+    }
+  })
+).then(resp => {
+  return resp.json()
+})
