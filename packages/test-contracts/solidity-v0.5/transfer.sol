7ba3c76e5e24812ded0d5aa745dac9659d5e57d4
sam <sam@freighttrust.com>
2021-02-04 11:50:19 -0800

    refactor(repo): refactor repo

--- packages/test-contracts/solidity-v0.5/transfer.sol
+++
@@ -1,7 +0,0 @@
-contract c {
-    uint x;
-    function f(address payable r) public payable {
-        r.transfer(1);
-        x = 2;
-    }
-}
--- packages/tests/wrapper.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-mkdir -p build-test; cp contracts/* tests/* build-test; perl -pi -e 's|../contracts/||' build-test/*; remix-tests build-test
-
